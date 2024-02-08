a4f97faae33401e9cc23000bdc09ce36de667ecf
AleKoza02 <alessio.cimma@gmail.com>
2024-02-08 19:57:32 +0100

    Python output immagine

--- raytracer_python/main.py
+++ raytracer_python/main.py
@@ -1,1 +1,7 @@
-print("Test GitHub")
+import numpy as np
+from utils import Image
+
+prova = np.array([0,0,0,255,255,255,255,0,0])
+
+post = Image(3,1)
+post.salva(prova)
---
+++ raytracer_python/utils.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+class Image:
+    def __init__(self, w, h) -> None:
+        self.w, self.h = w, h
+        self.max_color_value = 255
+        self.header = f"P6\n{self.w} {self.h}\n{self.max_color_value}\n"
+
+    def salva(self, data):
+        with open("raytracer_python/OUTPUT_python/python_1.ppm", "wb") as ppm_file:
+            ppm_file.write(bytearray(self.header, "ascii"))
+            ppm_file.write(data.astype(np.int8))
