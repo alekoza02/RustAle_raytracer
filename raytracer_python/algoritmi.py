b4186342fed4c9af7f5a208c7f480b54b97a2efb
AleKoza02 <alessio.cimma@gmail.com>
2024-02-08 22:02:08 +0100

    Shading normali sfere python

--- raytracer_python/algoritmi.py
+++ raytracer_python/algoritmi.py
@@ -20,6 +20,8 @@ class Collisioni:
                 risultato.distanza = risultato_iter
                 risultato.indice_sfera = indice
                 risultato.punto_colpito = raggio.origine + raggio.dir_iterazione * risultato.distanza
+                risultato.norma_colpito = risultato.punto_colpito - oggetto.origine
+                risultato.norma_colpito /= np.linalg.norm(risultato.norma_colpito)
 
         return risultato
 
--- raytracer_python/main.py
+++ raytracer_python/main.py
@@ -4,7 +4,7 @@ from camera import Camera
 from algoritmi import Collisioni
 from geometria import Scena
 
-W, H = 54, 54
+W, H = 540, 540
 
 post = Image(W,H)
 #camera = Camera(np.array([-3, -3, 4]), np.array([0.49960239, 0.49999984, -0.70738787]), np.array([-0.49999984, -0.50039761, -0.70682558]), np.array([0.70738787, -0.70682558, 0.]), np.pi/6)
@@ -22,7 +22,7 @@ for x in range(W):
         info_iter = tester.test_collisione(camera, scena.oggetti)
 
         if info_iter.colpito:
-            prova[x,y,:] = [255, 0, 0]
+            prova[x,y,:] = [(info_iter.norma_colpito[0] + 1) * 127, (info_iter.norma_colpito[1] + 1) * 127, (info_iter.norma_colpito[2] + 1) * 127]
         else:
             prova[x,y,:] = [abs(camera.dir_iterazione[0]) * 255, abs(camera.dir_iterazione[1]) * 255, abs(camera.dir_iterazione[2]) * 255]
 
