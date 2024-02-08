f506ac417e175d90dbe212d8704bec3c2cb3b4a1
AleKoza02 <alessio.cimma@gmail.com>
2024-02-08 23:20:50 +0100

    antialiasing python

--- raytracer_python/main.py
+++ raytracer_python/main.py
@@ -4,7 +4,8 @@ from camera import Camera
 from algoritmi import Collisioni
 from geometria import Scena
 
-W, H = 540, 540
+W, H = 128, 128
+SAMPLES = 8
 
 post = Image(W,H)
 #camera = Camera(np.array([-3, -3, 4]), np.array([0.49960239, 0.49999984, -0.70738787]), np.array([-0.49999984, -0.50039761, -0.70682558]), np.array([0.70738787, -0.70682558, 0.]), np.pi/6)
@@ -17,13 +18,14 @@ prova = np.zeros((W,H,3))
 
 for x in range(W):
     for y in range(H):
+        for samples in range(SAMPLES):
+            camera.genera_direzione(x,y,W,H)
+            info_iter = tester.test_collisione(camera, scena.oggetti)
 
-        camera.genera_direzione(x,y,W,H)
-        info_iter = tester.test_collisione(camera, scena.oggetti)
+            if info_iter.colpito:
+                prova[x,y,:] += [(info_iter.norma_colpito[0] + 1) * 127, (info_iter.norma_colpito[1] + 1) * 127, (info_iter.norma_colpito[2] + 1) * 127]
+            else:
+                prova[x,y,:] += [abs(camera.dir_iterazione[0]) * 255, abs(camera.dir_iterazione[1]) * 255, abs(camera.dir_iterazione[2]) * 255]
 
-        if info_iter.colpito:
-            prova[x,y,:] = [(info_iter.norma_colpito[0] + 1) * 127, (info_iter.norma_colpito[1] + 1) * 127, (info_iter.norma_colpito[2] + 1) * 127]
-        else:
-            prova[x,y,:] = [abs(camera.dir_iterazione[0]) * 255, abs(camera.dir_iterazione[1]) * 255, abs(camera.dir_iterazione[2]) * 255]
-
+prova /= SAMPLES
 post.salva(prova)
