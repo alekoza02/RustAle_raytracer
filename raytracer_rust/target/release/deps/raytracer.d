30a38868aa20a4f3e89525b247f22db34debc89d
AleKoza02 <alessio.cimma@gmail.com>
2024-02-09 18:03:21 +0100

    try merge

--- raytracer_rust/target/release/deps/raytracer.d
+++ raytracer_rust/target/release/deps/raytracer.d
@@ -1,6 +1,6 @@
-C:\Users\aless\OneDrive\git_locale\PyAle_raytracer\raytracer_rust\target\release\deps\raytracer.exe: src/main.rs src\camera.rs src\utils.rs src\geometria.rs src\algoritmi.rs
+C:\Users\aless\OneDrive\progetto_git_tracer\PyAle_raytracer\raytracer_rust\target\release\deps\raytracer.exe: src/main.rs src\camera.rs src\utils.rs src\geometria.rs src\algoritmi.rs
 
-C:\Users\aless\OneDrive\git_locale\PyAle_raytracer\raytracer_rust\target\release\deps\raytracer.d: src/main.rs src\camera.rs src\utils.rs src\geometria.rs src\algoritmi.rs
+C:\Users\aless\OneDrive\progetto_git_tracer\PyAle_raytracer\raytracer_rust\target\release\deps\raytracer.d: src/main.rs src\camera.rs src\utils.rs src\geometria.rs src\algoritmi.rs
 
 src/main.rs:
 src\camera.rs:
