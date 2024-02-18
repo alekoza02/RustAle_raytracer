7d906edec615a590c76abc7f797eb1cdb55c2d37
AleKoza02 <alessio.cimma@gmail.com>
2024-02-18 16:57:39 +0100

    Prova della UI (Slint)

--- src/test.rs
+++ src/test.rs
@@ -1,3 +1,16 @@
+slint::slint!{
+    import { Button, VerticalBox } from "std-widgets.slint";
+    export component App inherits Window {
+        property <int> counter: 1;
+
+        VerticalBox {
+            Text { text: "Hello -> " + counter; }
+            Button { text: "yay"; clicked => {counter += 1;}}
+        }
+    }
+}
+
 fn main() {
-    println!("Ciao tester!")
+    App::new().unwrap().run().unwrap();
+    println!("Tester")
 }
